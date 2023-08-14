0a59aac7c406238b0016a82f2470fe026d8dd925
Coex <email.coex@gmail.com>
2023-08-14 05:15:43 +0000

    abc

--- sample/http.js
+++ sample/http.js
@@ -1,10 +1,9 @@
-var express = require('express')
-var serveStatic = require('serve-static')
+var express = require("express");
+var serveStatic = require("serve-static");
 
-var staticBasePath = './';
- 
-var app = express()
- 
-app.use(serveStatic(staticBasePath))
-app.listen(8080)
-console.log('Listening on port 8080');
+var staticBasePath = "./";
+var app = express();
+
+app.use(serveStatic(staticBasePath));
+app.listen(8000);
+console.log("https://localhost/8000");
