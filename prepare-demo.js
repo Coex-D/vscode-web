8dce0f540153755f6c96f968dfb8134e484f4098
vipul-id-1 <143708102+vipul-id-1@users.noreply.github.com>
2023-09-13 04:49:38 +0000

    test webpack

--- prepare-demo.js
+++ prepare-demo.js
@@ -6,11 +6,13 @@ if (fs.existsSync("./demo/static")) {
   fs.rmdirSync("./demo/static", { recursive: true });
 }
 
-fse.copySync("./lib/extensions", "./demo/static/extensions");
-fse.copySync("./lib/node_modules", "./demo/static/node_modules");
-fse.copySync("./lib/out", "./demo/static/out");
+fse.copySync("./dist/extensions", "./demo/static/extensions");
+fse.copySync("./dist/node_modules", "./demo/static/node_modules");
+fse.copySync("./dist/out", "./demo/static/out");
 
-const webPlaygroundPath = './demo/static/extensions/vscode-web-playground';
+const webPlaygroundPath = "./demo/static/extensions/vscode-web-playground";
 
-child_process.execSync(`git clone https://github.com/microsoft/vscode-web-playground.git  ${webPlaygroundPath}`, {stdio: 'inherit'});
-
+child_process.execSync(
+  `git clone https://github.com/microsoft/vscode-web-playground.git  ${webPlaygroundPath}`,
+  { stdio: "inherit" }
+);
